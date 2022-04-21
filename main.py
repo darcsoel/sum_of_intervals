b8c1f34025f79e185cf37aa6f7bb30ef95126a7b
Ross <rostikvunarchuk@gmail.com>
2022-04-21 23:51:55 +0300

    update for negative indexes

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import sys
 
 
 def sum_of_intervals(intervals):
-    min_, max_ = sys.maxsize, 0
+    min_, max_ = sys.maxsize, -sys.maxsize - 1
 
     for (start, end) in intervals:
         min_, max_ = min(min_, start), max(max_, end)
 
+    diff = -1
+
+    if min_ < 0:
+        diff = abs(min_)
+        max_ += diff
+        min_ = 0
+
     elements = ["" for _ in range(max_ - min_)]
 
     for (start, end) in intervals:
-        elements[start - 1 : end - 1] = ["0" for _ in range(end - start)]
+        elements[start + diff : end + diff] = ["0" for _ in range((end + diff) - (diff + start))]
 
     return elements.count("0")
