440d36d226e8e88f05be7ee17a3409de19251f64
Ross <rostikvunarchuk@gmail.com>
2022-04-22 00:52:09 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-import sys
-
-
 def sum_of_intervals(intervals):
-    min_, max_ = sys.maxsize, -sys.maxsize - 1
+    result = set()
 
     for (start, end) in intervals:
-        min_, max_ = min(min_, start), max(max_, end)
+        result.update(set(range(start, end)))
 
-    diff = -1
-
-    if min_ < 0:
-        diff = abs(min_)
-        max_ += diff
-        min_ = 0
-
-    elements = ["" for _ in range(max_ - min_)]
-
-    for (start, end) in intervals:
-        elements[start + diff : end + diff] = ["0" for _ in range((end + diff) - (diff + start))]
-
-    return elements.count("0")
+    return len(result)
